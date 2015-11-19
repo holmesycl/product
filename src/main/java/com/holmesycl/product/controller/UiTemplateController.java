09d981f3a8eea2b22a216dbc0e29e7aec6f0518a
yecl <yecl@asiainfo.com>
2015-11-19 19:34:55 +0800

    templatecontroller

---
+++ src/main/java/com/holmesycl/product/controller/UiTemplateController.java
@@ -0,0 +1,11 @@
+package com.holmesycl.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/ui/template")
+public class UiTemplateController {
+
+}
